1ae406e19e6d09dea7f6f2e393eecbac53b7c89a
root <root@DESKTOP-M80G8FI.localdomain>
2020-11-19 22:00:35 +0800

    first commit

---
+++ test3.h
@@ -0,0 +1,31 @@
+#ifndef test3_h
+#define test3_h
+
+#include<stdio.h>
+#include<malloc.h>
+
+#define n 11
+#define m 3
+
+typedef int dataType;
+typedef struct node {
+      dataType data;
+      struct node *next;
+      int no;
+}LinkList, node;
+
+
+LinkList*CreateList();
+int Size(LinkList *l);
+void Insert(LinkList *l,int k,dataType x);
+void Delete(LinkList *l,int k);
+int Empty(LinkList *l);
+dataType GetData(LinkList *l,int k);
+node *Find(LinkList *l,dataType x);
+void Print(LinkList *l);
+void ClearList(LinkList *l);
+
+
+
+#endif
+

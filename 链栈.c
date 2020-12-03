ed04ac18481d5506c8a6326f5ee9456d4914f800
root <root@DESKTOP-M80G8FI.localdomain>
2020-12-03 12:32:05 +0800

    first commit

---
+++ 链栈.c
@@ -0,0 +1,45 @@
+#include "链栈.h"
+
+
+//初始化链栈
+LinkStack* InitStack() {
+     LinkStack *t = (LinkStack*)malloc(sizeof(LinkStack));
+     t->next = NULL;
+     return t;
+}
+
+//判断栈是否为空
+int Empty(LinkStack *s) {
+     return s->next == NULL;
+}
+
+//元素x进栈
+void Push(LinkStack *s, dataType x) {
+     struct node *t = (struct node*)malloc(sizeof(node));
+     t->data = x;
+     t->next = s->next;
+     s->next = t;
+}
+
+//出栈
+void Pop(LinkStack *s) {
+     if (Empty(s)) exit(1);
+     struct node *p = s->next;
+     s->next = p->next;
+     free(p);
+}
+
+//取栈顶元素
+dataType GetTop(LinkStack *s) {
+     return s->next->data;
+}
+
+int main() {
+     LinkStack *s = InitStack();
+     Push(s, 80);
+     Push(s, 90);
+     Pop(s);
+     Push(s, 70);
+     printf("%d\n", GetTop(s));
+}
+
